34ef2e235db149fa66fc89fcfbe20627ff4e9dc6
Kornel Tako <takokornel@gmail.com>
2023-04-15 17:16:55 +0200

    cleanup

--- src/models/user.ts
+++
@@ -1,8 +0,0 @@
-import { UserInfo } from 'firebase/auth';
-
-export interface User extends UserInfo {
-  firstName?: string | null;
-  lastName?: string | null;
-  loggedIn?: boolean;
-  role?: string | null;
-}
